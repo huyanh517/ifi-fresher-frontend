fbe7f68d41d67763389b9697b9923f3c811675a9
Huy Anh <huyanh151998@gmail.com.vn>
2021-10-11 14:02:19 +0700

    create file

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-undef */
 $(document).ready(function () {
     const $body = $('body');
-    $.get('/components/header.html', data => {$body.append(data); $body.append(data)})
+    $.get('/components/header.html', data => {$body.append(data)})
+    $.get('/components/main/main-home.html', data => {$body.append(data)})
+    $.get('components/footer.html', data => {$body.append(data)})
 });
