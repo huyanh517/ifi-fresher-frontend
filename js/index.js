4ef14de7f1e5e7f646c2bb64fe69bf00f6a01660
Huy Anh <huyanh151998@gmail.com.vn>
2021-10-13 15:43:41 +0700

    ifi day 2

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@
 $(document).ready(function () {
     const $body = $('body');
     $.get('/components/header.html', data => {$body.append(data)})
-    // $.get('/components/main/main-home.html', data => {$body.append(data)})
+    $.get('/components/main/main-home.html', data => {$body.append(data)})
     // $.get('components/footer.html', data => {$body.append(data)})
 });
+
+
