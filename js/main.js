1ebcba248b353119eeae7edb9119ca363981e6cb
Huy Anh <huyanh151998@gmail.com.vn>
2021-10-14 09:55:15 +0700

    day3

--- js/main.js
+++ js/main.js
@@ -4,18 +4,17 @@ $(document).ready(function () {
 
 function scrollTop() {
     let windowHeight = $(window).scrollTop()
-
     if (windowHeight > 250) {
         $('.btn-scroll').css('opacity', '1')
     } else {
         $('.btn-scroll').css('opacity', '0')
     }
-
-    $('.btn-scroll').click(() => {
-        $(document.documentElement).animate({
-            scrollTop: 0
-        }, 500)
-        console.log('Test')
-        return false
-    })
 }
+
+$('.btn-scroll').click(() => {
+    $(document.documentElement).animate({
+        scrollTop: 0
+    }, 500)
+    console.log('Test')
+    return false
+})
