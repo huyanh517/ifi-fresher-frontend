beb1259ce0452cfe8718781b00ece9b09bba1933
Huy Anh <huyanh151998@gmail.com.vn>
2021-10-14 09:35:26 +0700

    day3

---
+++ js/common.js
@@ -0,0 +1,56 @@
+$(document).ready(() => {
+    //SLIDER
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        nav: false,
+        mouseDrag: false,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplaySpeed: 1000,
+        responsive: {
+            0: {
+                items: 1
+            },
+        }
+    })
+
+    // MOBILE MENU
+    $('.button').click(() => {
+        if ($('.menu-left').hasClass('open')) {
+            $('.menu-left').removeClass('open')
+            $('.menu-left').addClass('close')
+        } else {
+            $('.menu-left').removeClass('close')
+            $('.menu-left').addClass('open')
+        }
+    })
+
+    function checkWidth() {
+        let windowSize = $(window).width()
+        if (windowSize > 767) {
+            $('.menu-left').removeClass('close')
+            $('.menu-left').addClass('open')
+        } else {
+            $('.menu-left').removeClass('open')
+        }
+    }
+    checkWidth()
+    $(window).resize(checkWidth)
+
+    // HEADER SCROLL
+    $(window).scroll(checkHeight)
+
+    function checkHeight() {
+        let windowHeight = $(window).scrollTop()
+        if (windowHeight > 210) {
+            $('.header__top').css('position', 'fixed')
+        } else {
+            $('.header__top').css({
+                'position': 'sticky',
+                'z-index': 200,
+                'top': 0
+            })
+        }
+    }
+
+})
